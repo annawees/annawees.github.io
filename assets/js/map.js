97c823cbf8e33c8ee0e3d4d1c66ebe9b3fd4fc5d
annawees <anne@cilantrolime.com>
2017-02-09 22:16:17 -0600

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,6 @@
-// *
-// * Add multiple markers
-// * 2013 - en.marnoto.com
-// *
-
-// necessary variables
 var map;
 var infoWindow;
 
-// markersData variable stores the information necessary to each marker
 var markersData = [
    {
       lat: 40.6386333,
@@ -15,7 +8,7 @@ var markersData = [
       name: "Camping Praia da Barra",
       address1:"Rua Diogo Cão, 125",
       address2: "Praia da Barra",
-      postalCode: "3830-772 Gafanha da Nazaré" // don't insert comma in the last item of each marker
+      postalCode: "3830-772 Gafanha da Nazaré"
    },
    {
       lat: 40.59955,
@@ -23,7 +16,7 @@ var markersData = [
       name: "Camping Costa Nova",
       address1:"Quinta dos Patos, n.º 2",
       address2: "Praia da Costa Nova",
-      postalCode: "3830-453 Gafanha da Encarnação" // don't insert comma in the last item of each marker
+      postalCode: "3830-453 Gafanha da Encarnação"
    },
    {
       lat: 40.6247167,
@@ -31,8 +24,8 @@ var markersData = [
       name: "Camping Gafanha da Nazaré",
       address1:"Rua dos Balneários do Complexo Desportivo",
       address2: "Gafanha da Nazaré",
-      postalCode: "3830-225 Gafanha da Nazaré" // don't insert comma in the last item of each marker
-   } // don't insert comma in the last item
+      postalCode: "3830-225 Gafanha da Nazaré"
+   }
 ];
 
 
@@ -45,28 +38,20 @@ function initialize() {
 
    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-   // a new Info Window is created
    infoWindow = new google.maps.InfoWindow();
 
-   // Event that closes the Info Window with a click on the map
    google.maps.event.addListener(map, 'click', function() {
       infoWindow.close();
    });
 
-   // Finally displayMarkers() function is called to begin the markers creation
    displayMarkers();
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
-// This function will iterate over markersData array
-// creating markers with createMarker function
 function displayMarkers(){
 
-   // this variable sets the map bounds according to markers position
    var bounds = new google.maps.LatLngBounds();
    
-   // for loop traverses markersData array calling createMarker function for each marker 
    for (var i = 0; i < markersData.length; i++){
 
       var latlng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
@@ -77,16 +62,12 @@ function displayMarkers(){
 
       createMarker(latlng, name, address1, address2, postalCode);
 
-      // marker position is added to bounds variable
       bounds.extend(latlng);  
    }
 
-   // Finally the bounds variable is used to set the map bounds
-   // with fitBounds() function
    map.fitBounds(bounds);
 }
 
-// This function creates each marker and it sets their Info Window content
 function createMarker(latlng, name, address1, address2, postalCode){
    var marker = new google.maps.Marker({
       map: map,
@@ -94,22 +75,16 @@ function createMarker(latlng, name, address1, address2, postalCode){
       title: name
    });
 
-   // This event expects a click on a marker
-   // When this event is fired the Info Window content is created
-   // and the Info Window is opened.
    google.maps.event.addListener(marker, 'click', function() {
       
-      // Creating the content to be inserted in the infowindow
       var iwContent = '<div id="iw_container">' +
             '<div class="iw_title">' + name + '</div>' +
          '<div class="iw_content">' + address1 + '<br />' +
          address2 + '<br />' +
          postalCode + '</div></div>';
       
-      // including content to the Info Window.
       infoWindow.setContent(iwContent);
 
-      // opening the Info Window in the current map and at the current marker location.
       infoWindow.open(map, marker);
    });
 }
