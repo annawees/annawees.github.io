585f62f5f203a8ecbb665bf728a03539b22f9792
annawees <anne@cilantrolime.com>
2017-02-10 08:03:50 -0600

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,58 +1,24 @@
-function initMap() {
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'roadmap'
-    };
-                    
-    map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
-    map.setTilt(50);
-        
-    var markers = [
-        ['Brooklyn Museum, NY', 40.671531, -73.963588],
-        ['Brooklyn Public Library, NY', 40.672587, -73.968146],
-        ['Prospect Park Zoo, NY', 40.665588, -73.965336]
-    ];
-                        
-    var infoWindowContent = [
-        ['<div class="info_content">' +
-        '<h3>Brooklyn Museum</h3>' +
-        '<p>The Brooklyn Museum is an art museum located in the New York City borough of Brooklyn.</p>' + '</div>'],
-        ['<div class="info_content">' +
-        '<h3>Brooklyn Public Library</h3>' +
-        '<p>The Brooklyn Public Library (BPL) is the public library system of the borough of Brooklyn, in New York City.</p>' +
-        '</div>'],
-        ['<div class="info_content">' +
-        '<h3>Prospect Park Zoo</h3>' +
-        '<p>The Prospect Park Zoo is a 12-acre (4.9 ha) zoo located off Flatbush Avenue on the eastern side of Prospect Park, Brooklyn, New York City.</p>' +
-        '</div>']
-    ];
-        
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
-    
-    for( i = 0; i < markers.length; i++ ) {
-        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers[i][0]
-        });
-          
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
-
-        map.fitBounds(bounds);
-    }
-
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(14);
-        google.maps.event.removeListener(boundsListener);
-    });
-    
-}
-google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addDomListener(window, 'load', init);
+         
+             function init() {
+                 var mapOptions = {
+                     zoom: 12,
+                     center: new google.maps.LatLng(15.881473, 108.344981),
+               	    styles:[{"featureType": "water","elementType":"geometry","stylers":[{"color": "#e9e9e9"},{"lightness": 17}]},{"featureType": "landscape","elementType": "geometry","stylers": [{"color": "#f5f5f5"},{"lightness": 20}]},{"featureType": "road.highway","elementType": "geometry.fill","stylers": [{"color": "#ffffff"},{"lightness": 17}]},{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{"color": "#ffffff"},{"lightness": 29},{"weight": 0.2}]},{"featureType": "road.arterial","elementType": "geometry","stylers": [{"color": "#ffffff"},{"lightness": 18}]},{"featureType": "road.local","elementType": "geometry","stylers": [{"color": "#ffffff"},{"lightness": 16}]},{"featureType": "poi","elementType": "geometry","stylers": [{"color": "#f5f5f5"},{"lightness": 21}]},{"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#dedede"},{"lightness": 21}]},{"elementType": "labels.text.stroke","stylers": [{"visibility": "on"},{"color": "#ffffff"},{"lightness": 16}]},{"elementType": "labels.text.fill","stylers": [{"saturation": 36},{"color": "#333333"},{"lightness": 40}]},{"elementType": "labels.icon","stylers": [{"visibility": "off"}]},{"featureType": "transit","elementType": "geometry","stylers": [{"color": "#f2f2f2"},{"lightness": 19}]},{"featureType": "administrative","elementType": "geometry.fill","stylers": [{"color": "#fefefe"},{"lightness": 20}]},{"featureType": "administrative","elementType": "geometry.stroke","stylers": [{"color": "#fefefe"},{"lightness": 17},{"weight": 1.2}]}]
+ 		    };
+ 
+                 var mapElement = document.getElementById('map');
+ 
+                 var map = new google.maps.Map(mapElement, mapOptions);
+ 
+                 var marker = new google.maps.Marker({
+                      position: new google.maps.LatLng(15.881473, 108.344981),
+                      map: map,
+		              title: 'Orivy',});
+		 
+		 var marker = new google.maps.Marker({
+                      position: new google.maps.LatLng(15.891473, 108.344981),
+                      map: map,});
+		    
+              }
+	
