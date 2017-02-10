f719cab81d65c7b95be753db7854b8cfab346a0a
annawees <anne@cilantrolime.com>
2017-02-10 07:54:44 -0600

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,90 +1,60 @@
-var map;
-var infoWindow;
+<script>
+function initMap() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
+    map.setTilt(50);
+        
+    var markers = [
+        ['Brooklyn Museum, NY', 40.671531, -73.963588],
+        ['Brooklyn Public Library, NY', 40.672587, -73.968146],
+        ['Prospect Park Zoo, NY', 40.665588, -73.965336]
+    ];
+                        
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3>Brooklyn Museum</h3>' +
+        '<p>The Brooklyn Museum is an art museum located in the New York City borough of Brooklyn.</p>' + '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Brooklyn Public Library</h3>' +
+        '<p>The Brooklyn Public Library (BPL) is the public library system of the borough of Brooklyn, in New York City.</p>' +
+        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Prospect Park Zoo</h3>' +
+        '<p>The Prospect Park Zoo is a 12-acre (4.9 ha) zoo located off Flatbush Avenue on the eastern side of Prospect Park, Brooklyn, New York City.</p>' +
+        '</div>']
+    ];
+        
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+          
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
 
-var markersData = [
-   {
-      lat: 40.6386333,
-      lng: -8.745,
-      name: "Camping Praia da Barra",
-      address1:"Rua Diogo Cão, 125",
-      address2: "Praia da Barra",
-      postalCode: "3830-772 Gafanha da Nazaré"
-   },
-   {
-      lat: 40.59955,
-      lng: -8.7498167,
-      name: "Camping Costa Nova",
-      address1:"Quinta dos Patos, n.º 2",
-      address2: "Praia da Costa Nova",
-      postalCode: "3830-453 Gafanha da Encarnação"
-   },
-   {
-      lat: 40.6247167,
-      lng: -8.7129167,
-      name: "Camping Gafanha da Nazaré",
-      address1:"Rua dos Balneários do Complexo Desportivo",
-      address2: "Gafanha da Nazaré",
-      postalCode: "3830-225 Gafanha da Nazaré"
-   }
-];
+        map.fitBounds(bounds);
+    }
 
-
-function initialize() {
-   var mapOptions = {
-      center: new google.maps.LatLng(40.601203,-8.668173),
-      zoom: 9,
-      mapTypeId: 'roadmap',
-   };
-
-   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-   infoWindow = new google.maps.InfoWindow();
-
-   google.maps.event.addListener(map, 'click', function() {
-      infoWindow.close();
-   });
-
-   displayMarkers();
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
 }
-google.maps.event.addDomListener(window, 'load', initialize);
-
-function displayMarkers(){
-
-   var bounds = new google.maps.LatLngBounds();
-   
-   for (var i = 0; i < markersData.length; i++){
-
-      var latlng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
-      var name = markersData[i].name;
-      var address1 = markersData[i].address1;
-      var address2 = markersData[i].address2;
-      var postalCode = markersData[i].postalCode;
-
-      createMarker(latlng, name, address1, address2, postalCode);
-
-      bounds.extend(latlng);  
-   }
-
-   map.fitBounds(bounds);
-}
-
-function createMarker(latlng, name, address1, address2, postalCode){
-   var marker = new google.maps.Marker({
-      map: map,
-      position: latlng,
-      title: name
-   });
-
-   google.maps.event.addListener(marker, 'click', function() {
-      
-      var iwContent = '<div id="iw_container">' +
-            '<div class="iw_title">' + name + '</div>' +
-         '<div class="iw_content">' + address1 + '<br />' +
-         address2 + '<br />' +
-         postalCode + '</div></div>';
-      
-      infoWindow.setContent(iwContent);
-
-      infoWindow.open(map, marker);
-   });
-}
+google.maps.event.addDomListener(window, 'load', initMap);
+</script>
